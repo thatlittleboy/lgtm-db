d2a0c6e275ef002b5bbf96e9a6c265788d5ae6cd
Jeremy Goh <jeremy_gsk@outlook.com>
2022-06-04 14:23:44 +0800

    release: v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="lgtm-db",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/thatlittleboy/lgtm-db",
     install_requires=[
         "pyyaml",
