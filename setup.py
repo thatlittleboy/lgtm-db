11e29af23d3e2c2c1b266d99b528ba0c8e1f409c
Jeremy Goh <jeremy_gsk@outlook.com>
2022-06-04 14:52:43 +0800

    chore: move to setup.cfg

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="lgtm-db",
-    version="1.0.1",
-    url="https://github.com/thatlittleboy/lgtm-db",
-    install_requires=[
-        "pyyaml",
-        "importlib_resources; python_version < '3.9'",
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-    entry_points={
-        "console_scripts": [
-            "lgtm-db = lgtm_db:main",
-        ],
-    },
-)
+setup()
