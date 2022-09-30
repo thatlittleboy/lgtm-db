6b323fc0a74a7eb0a3e3d9ba52b3c72bc15750f1
Jeremy Goh <jeremy_gsk@outlook.com>
2022-09-30 22:22:02 +0800

    add tests for matcher

---
+++ tests/test_matcher.py
@@ -0,0 +1,25 @@
+from lgtm_db.matcher import Matcher
+
+
+def test_matcher_type():
+    # regex
+    assert Matcher.is_regex_pattern("^$")
+    assert Matcher.is_regex_pattern(r"^good.+$")
+
+    # string
+    assert not Matcher.is_regex_pattern("")
+    assert not Matcher.is_regex_pattern(r"lgtm_gif")
+    assert not Matcher.is_regex_pattern(r"$hahah$")
+
+
+def test_match_for_regex():
+    assert Matcher(r"^good.+$").match("good-boy")
+    assert Matcher(r"^good.boy$").match("good-boy")
+    assert not Matcher(r"^good$").match("good-boy")
+
+
+def test_match_for_string():
+    """When matching on string, should be a "substring" logic."""
+    assert Matcher("good").match("good-boy")
+    assert not Matcher("bad").match("good-boy")
+    assert not Matcher("good.boy").match("good-boy")
